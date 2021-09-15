d76abe83911d1216947d66aa43418479aa647d6f
s2lo <90177845+s2lo@users.noreply.github.com>
2021-09-15 13:13:12 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+	
+	int q;
+	char w;
+	unsigned char e;
+	short r;
+	unsigned short t;
+	unsigned int y;
+	long u;
+	unsigned long i;
+	float o;
+	double p;
+	long double a;
+	
+	int s = 1;
+	char d = 2;
+	unsigned char f = 3;
+	short g = 4;
+	unsigned short h = 5;
+	unsigned int j= 6;
+	long k = 7;
+	unsigned long l = 8;
+	float z = 9;
+	double x = 10;
+	long double c = 11;
+
+
+
+	printf("%d\n", s);
+	printf("%d\n", d);
+	printf("%d\n", f);
+	printf("%d\n", g);
+	printf("%d\n", h);
+	printf("%d\n", j);
+	printf("%d\n", k);
+	printf("%d\n", l);
+	printf("%f\n", z);
+	printf("%lf\n", x);
+	printf("%lf\n", c);
+	return 0;
+
+}
