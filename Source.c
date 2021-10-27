d270d4f0eb0e59f3d8ffcd94e470c122bfe5e027
s2lo <90177845+s2lo@users.noreply.github.com>
2021-10-27 11:42:27 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>;
-
 #include <stdlib.h>;
 
 int main()
 {
-	int n = 0;
+	int n,k = 0;
 	long double min = 0.0;
 	long double max = 0.0;
 	int i = 0;
@@ -15,17 +14,18 @@ int main()
 	printf("N=\t");
 	scanf_s("%d", &n);
 
-		if (n <= 0)
-		{
-			printf("error_n<=0");
-				return 0;
-		}
-
 	printf("Min=\t");
 	scanf_s("%lf", &min);
 
 	printf("Max=\t");
 	scanf_s("%lf", &max);
+
+		if (n <= 0)
+		{
+			printf("error_n<=0");
+			return 0;
+		}
+
 
 		if (min > max)
 		{
@@ -38,22 +38,35 @@ int main()
 	mas = (double*)malloc(n * sizeof(double));
 	ost = (double*)malloc(n * sizeof(double));
 	double sum = 0.0;
+
+
 	for (i = 0; i < n; i++)
 	{
-		;
+		
 		mas[i] = (((double)rand()) / RAND_MAX) * (max - min) + min;
-		ost[i] = (mas[i] - (int)mas[i]) * 1000;
+		ost[i] = (mas[i] - (int)mas[i]) * 1000000;
 		ost[i] = (int)ost[i];
-		printf("mas[%d]=%lf  ost[%d]=%lf\n", i, mas[i], i, ost[i]);
-
-		if (i != ost[i])
-		{
-			sum = sum + mas[i];
-			printf("%lf\n", sum);
-		}
+		printf("mas[%d]=%lf\n", i, mas[i]);
 	}
 	
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < n; k++)
+		{
+			if (i == ost[k])
+			{
+				sum -= mas[k];
+				mas[k] = 0.0;
+			}
+			
+		}
+		sum += mas[i];
+	}
+
+	printf("%lf", sum);
+	free(mas);
+	free(ost);
 	return 0;
-	 free(mas);
+
 
 }
